70e81bf0b4d4f90275b6745ec83dbedcfaa1e623
igh01gi <igh01pr@outlook.com>
2024-01-01 04:37:32 +0900

    인프론 C#게임서버 Listner 까지 코딩
    서버측 Accept부분을 논 블로킹(비동기) 방식으로 구현했음

---
+++ ServerCore/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ServerCore
+{
+    class Program
+    {
+        private static Listener _listener = new Listener();
+
+        static void OnAcceptHandler(Socket clientSocket)
+        {
+            try
+            {
+                //받는다
+                byte[] recvBuff = new byte[1024];
+                int recvBytes = clientSocket.Receive(recvBuff);
+                string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
+                Console.WriteLine($"[From Client]{recvData}");
+
+                //보낸다
+                byte[] sendBuff = Encoding.UTF8.GetBytes("Welcome to MMORPG SERVER !");
+                clientSocket.Send(sendBuff);
+
+                //쫓아낸다
+                clientSocket.Shutdown(SocketShutdown.Both);
+                clientSocket.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            //DNS
+            string host = Dns.GetHostName();
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
+
+            _listener.Init(endPoint, OnAcceptHandler); //누가 들어오면 OnAcceptHandler로 알려줘
+            Console.WriteLine("Listening...");
+
+            while (true)
+            {
+                //프로그램 종료 방지용 임시
+            }
+        }
+    }
+}
