4e233dcb1819ae49d4e1eea91737dccb6262e4eb
igh01gi <igh01pr@outlook.com>
2024-01-04 05:14:05 +0900

    패킷직렬화 조금들음

---
+++ DummyClient/ServerSession.cs
@@ -0,0 +1,62 @@
+﻿using ServerCore;
+using System.Net;
+using System.Text;
+
+namespace DummyClient;
+
+class Packet
+{
+    public int size;
+    public int packetId = 4;
+}
+
+class PlayerInfoReq : Packet //플레이어 정보를 알고싶어서 서버로 보내는 패킷 (request)
+{
+    public long playerId;
+}
+
+class PlayerInfoOk : Packet //플레이어 정보를 서버한테 받았을때의 정보 패킷
+{
+    public int hp;
+    public int attack;
+}
+
+class ServerSession : Session
+{
+    public override void OnConnected(EndPoint endPoint)
+    {
+        Console.WriteLine($"OnConnected: {endPoint}");
+
+        Packet packet = new Packet() { size = 4, packetId = 7 };
+
+        //보낸다
+        for (int i = 0; i < 5; i++)
+        {
+            ArraySegment<byte> openSegment = SendBufferHelper.Open(4096);
+            byte[] buffer = BitConverter.GetBytes(packet.size);
+            byte[] buffer2 = BitConverter.GetBytes(packet.packetId);
+            Array.Copy(buffer, 0, openSegment.Array, openSegment.Offset, buffer.Length);
+            Array.Copy(buffer2, 0, openSegment.Array, openSegment.Offset + buffer.Length, buffer2.Length);
+            ArraySegment<byte> sendBuff = SendBufferHelper.Close(packet.size);
+
+            Send(sendBuff);
+        }
+    }
+
+    public override void OnDisconnected(EndPoint endPoint)
+    {
+        Console.WriteLine($"OnDisconnected: {endPoint}");
+    }
+
+    public override int OnRecv(ArraySegment<byte> buffer)
+    {
+        string recvData = Encoding.UTF8.GetString(buffer.Array, buffer.Offset, buffer.Count);
+        Console.WriteLine($"[From Server] {recvData}");
+        return buffer.Count;
+    }
+
+    public override void OnSend(int numOfBytes)
+    {
+        Console.WriteLine($"Transferred bytes: {numOfBytes}");
+    }
+}
--- Server/Program.cs
+++ Server/Program.cs
@@ -4,52 +4,7 @@ using System.Text;
 
 namespace Server;
 
-class Packet
-{
-    public int size;
-    public int packetId;
-}
 
-
-
-class GameSession : PacketSession
-{
-    public override void OnConnected(EndPoint endPoint)
-    {
-        Console.WriteLine($"OnConnected: {endPoint}");
-
-        /*Packet packet = new Packet() { size=4, packetId = 10};
-
-        ArraySegment<byte> openSegment = SendBufferHelper.Open(4096);
-        byte[] buffer = BitConverter.GetBytes(packet.size);
-        byte[] buffer2 = BitConverter.GetBytes(packet.packetId);
-        Array.Copy(buffer,0, openSegment.Array, openSegment.Offset, buffer.Length);
-        Array.Copy(buffer2,0, openSegment.Array, openSegment .Offset+ buffer.Length,buffer2.Length);
-        ArraySegment<byte> sendBuff =SendBufferHelper.Close(packet.size);
-
-
-        Send(sendBuff);*/
-        Thread.Sleep(5000);
-        Disconnect();
-    }
-
-    public override void OnDisconnected(EndPoint endPoint)
-    {
-        Console.WriteLine($"OnDisconnected: {endPoint}");
-    }
-
-    public override void OnRecvPacket(ArraySegment<byte> buffer)
-    {
-        ushort size = BitConverter.ToUInt16(buffer.Array, buffer.Offset);
-        ushort id = BitConverter.ToUInt16(buffer.Array,buffer.Offset+2);
-        Console.WriteLine($"RecvPacketId: {id}. Size:{size}");
-    }
-
-    public override void OnSend(int numOfBytes)
-    {
-        Console.WriteLine($"Transferred bytes: {numOfBytes}");
-    }
-}
 
 class Program
 {
@@ -64,7 +19,7 @@ class Program
         IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
 
 
-        _listener.Init(endPoint, () => { return new GameSession(); }); //누가 들어오면 OnAcceptHandler로 알려줘
+        _listener.Init(endPoint, () => { return new ClientSession(); }); //누가 들어오면 OnAcceptHandler로 알려줘
         Console.WriteLine("Listening...");
 
         while (true)
