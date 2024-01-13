9c2ddccfc013c6e06c1d5dba858b94563e1ca338
igh01gi <igh01pr@outlook.com>
2024-01-13 18:20:18 +0900

    유니티 연동#3 까지
    패킷포맷에 움직임, 입장, 나가기, 등등 기존의 채팅패킷을 지우고 추가하였음

--- DummyClient/Session/SessionManager.cs
+++ DummyClient/Session/SessionManager.cs
@@ -7,6 +7,7 @@ public class SessionManager
     
     List<ServerSession> _sessions = new List<ServerSession>();
     object _lock = new object();
+    Random _rand = new Random();
 
     public void SendForEach()
     {
@@ -14,11 +15,12 @@ public class SessionManager
         {
             foreach (ServerSession session in _sessions)
             {
-                C_Chat chatPacket = new C_Chat();
-                chatPacket.chat = $"Hello Server! I'm Client";
-                ArraySegment<byte> segment = chatPacket.Write();
-
-                session.Send(segment);
+                C_Move movePacket = new C_Move();
+                movePacket.posX = _rand.Next(-50,50);
+                movePacket.posY = 0;
+                movePacket.posZ = _rand.Next(-50,50);
+                
+                session.Send(movePacket.Write());
             }
         }
     }
